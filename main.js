2ac71859e855c96074d39fc0fafc08fe976b78dc
Anas Chowdhury <chowdhuryanas10@gmail.com>
2023-12-27 17:30:49 -0700

    Task name and due date are now shown to the screen, and images have been added

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ function addTask() {
     let task = document.getElementById("taskName").value;
     let date = document.getElementById("date").value;
     
-    list.innerHTML += `<li>${task}</li>`;
+    list.innerHTML += 
+    `
+    <div class="task-item">
+        <div class="unchecked-circle">
+            <img src="images/unchecked.png">
+        </div>        
+        <div class="task-name">
+            <h4>${task}</h4>
+        </div>
+        <div class = "due-date">
+            <h4>${date}</h4>
+        </div>
+    </div>
+    `;
+
+
     /*
     document.getElementById("taskName").value = "";
     document.getElementById("date").value = "";
